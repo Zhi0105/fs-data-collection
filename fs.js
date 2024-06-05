3e69c2f00f59b71ddcfe8481a2cc5e9dcdc42167
Argie <argie.b@flowerstore.ph>
2024-06-05 13:03:14 +0800

    test hub

--- fs.js
+++ fs.js
@@ -141,17 +141,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         // CHANGE HUB ACTION TYPE START
-
-        const chpayload = {}
-        const selectElement = document.querySelector('select[name="hub_id"]');
-        if (selectElement) {
-            selectElement.addEventListener('change',(e) => {
-                chpayload.session_id = getCookie("session_analytics_id"),
-                chpayload.action_key = 'change hub',
-                chpayload.hub_id =  e.target.value,
-                console.log("@CH:", chpayload)
-            });
-        }
+        console.log(document.querySelector('select[name="hub_id"]'))
+        // const chpayload = {}
+        // const selectElement = document.querySelector('select[name="hub_id"]');
+        // if (selectElement) {
+        //     selectElement.addEventListener('change',(e) => {
+        //         chpayload.session_id = getCookie("session_analytics_id"),
+        //         chpayload.action_key = 'change hub',
+        //         chpayload.hub_id =  e.target.value,
+        //         console.log("@CH:", chpayload)
+        //     });
+        // }
      
         // for (let i = 0; i < hub.length; i+=1) {
         //     hub[i].addEventListener('change', (e) => {
